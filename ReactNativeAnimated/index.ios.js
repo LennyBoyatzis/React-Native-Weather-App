6323850c5f419760e757cdcf1ff100ef288d1435
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-04-18 19:46:12 +1000

    floating heart

--- ReactNativeAnimated/index.ios.js
+++ ReactNativeAnimated/index.ios.js
@@ -30,32 +30,52 @@ class ReactNativeAnimated extends Component {
         };
     }
 
+    componentWillMount() {
+        this._yAnimation = this.state.position.interpolate({
+            inputRange: [NEGATIVE_END_Y, 0],
+            outputRange: [ANIMATION_END_Y, 0]
+        })
+        this._opacityAnimation = this._yAnimation.interpolate({
+            inputRange: [0, ANIMATION_END_Y],
+            outputRange: [1, 0]
+        })
+        this._scaleAnimation = this._yAnimation.interpolate({
+            inputRange: [0,15,30],
+            outputRange: [0,1.2,1],
+            extrapolate: 'clamp'
+        })
+        this._xAnimation = this._yAnimation.interpolate({
+            inputRange: [0, ANIMATION_END_Y/2, ANIMATION_END_Y],
+            outputRange: [0,15,0]
+        })
+        this._rotateAnimation = this._yAnimation.interpolate({
+            inputRange: [0, ANIMATION_END_Y/4, ANIMATION_END_Y/3, ANIMATION_END_Y/2, ANIMATION_END_Y],
+            outputRange: ['0deg', '-10deg', '0deg', '10deg', '0deg']
+        })
+    }
+
     componentDidMount() {
         Animated.timing(this.state.position, {
             duration: 2000,
             toValue: NEGATIVE_END_Y
         }).start()
-
-        this._yAnimation = this.state.position.interpolate({
-            inputRange: [NEGATIVE_END_Y, 0],
-            outputRange: [ANIMATION_END_Y, 0]
-        });
-        this._opacityAnimation = this._yAnimation.interpolate({
-            inputRange: [0, ANIMATION_END_Y],
-            outputRange: [1, 0]
-        })
     }
 
     getHeartAnimationStyle() {
+
         return {
             transform: [
-                {translateY: this.state.position}
+                {translateY: this.state.position},
+                {translateX: this._xAnimation},
+                {scale: this._scaleAnimation},
+                {rotate: this._rotateAnimation}
             ],
             opacity: this._opacityAnimation
         }
     }
 
     render() {
+        console.log("getHeartAnimationStyle", this.getHeartAnimationStyle())
         return (
             <View style={styles.container}>
                 <Animated.View style={[styles.heartWrap, this.getHeartAnimationStyle()]}>
